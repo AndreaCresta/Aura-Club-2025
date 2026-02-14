42c0475badd3347b4b328cfbf498995ced1c506c
AndreaCresta <andrea.cresta1997@gmail.com>
2026-02-14 12:15:46 +0100

    chore: Update project base path and homepage to 'Aura-Club-2025'.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Aura-Club-Terni/",
+  base: "/Aura-Club-2025/",
   build: {
     target: "es2015",
   },
